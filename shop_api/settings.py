e1e1112d21262e6eb43b405a9c94b5529d2b4c6e
diorbek90 <diorbek909090@gmail.com>
2025-05-27 23:47:15 +0600

    gitignore

--- shop_api/settings.py
+++ shop_api/settings.py
@@ -84,8 +84,12 @@ WSGI_APPLICATION = 'shop_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'db_product',
+        'USER': 'admin_product',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
